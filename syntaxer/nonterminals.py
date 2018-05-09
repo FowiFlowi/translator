e8f2fdb1096e600185c71e94975c5726225d5605
FowiFlowi <fowiflowi@gmail.com>
2018-05-10 02:16:53 +0300

    omg

--- syntaxer/nonterminals.py
+++ syntaxer/nonterminals.py
@@ -16,17 +16,30 @@ class SyntaxerError(Exception):
     self.value = value
     self.expected = expected
 
-def nonTerminal(terminalFunc):
-  def wrapper():
+def errorHandler(e):
+  if e.expected == None:
+    print '{}ERROR:{} {}'.format(bcolors.FAIL, bcolors.ENDC, e.value)
+  else:
+    token = e.value
+    print ('{}ERROR:{} Unexpected token \'{}{}{}\' on position {}{}, {}{}. Expected \'{}{}{}\''
+      .format(bcolors.FAIL, bcolors.ENDC, bcolors.BOLD, token['name'], bcolors.ENDC,
+        bcolors.BOLD, token['line'], token['pos'], bcolors.ENDC, bcolors.BOLD, e.expected, bcolors.ENDC))
+  return root
+
+def nonTerminal(nonTerminalFunc):
+  def wrapper(inputTokens = None):
+    if inputTokens:
+      global tokens
+      tokens = inputTokens
     global currNode
     parentNode = currNode
-    addNode(terminalFunc.__name__)
-    result = terminalFunc()
+    addNode(nonTerminalFunc.__name__)
+    result = nonTerminalFunc()
     currNode = parentNode
     return result
   return wrapper
 
-def getToken():
+def terminal(expected, raiseError = True, tokenField = 'name', expectedMsg = None):
   global currTokenIndx
   if currTokenIndx >= len(tokens):
     raise SyntaxerError('Unexpected end of file')
@@ -35,9 +48,21 @@ def getToken():
   token['terminal'] = True
   node = Node(token)
   currNode.add(node)
-
   currTokenIndx += 1
-  return token
+
+  value = token[tokenField]
+  if isinstance(expected, list):
+    claim = expected if callable(expected) else value in expected
+    if not claim and raiseError:
+      raise SyntaxerError(token, 'one of them: ' + ', '.join(expectedMsg if expectedMsg else expected))
+    else:
+      return claim
+  else:
+    claim = expected if callable(expected) else value == expected
+    if not claim and raiseError:
+      raise SyntaxerError(token, expectedMsg if expectedMsg else expected)
+    else:
+      return claim
 
 def rollback(node):
   global currTokenIndx
@@ -57,54 +82,26 @@ def addNode(name, code = None):
     currNode = node
   return True
 
-def program(inputTokens):
-  global tokens
-  tokens = inputTokens
-  addNode('program')
-
-  if (len(tokens) < 3):
-    raise SyntaxerError('Unexpected end of file')
+@nonTerminal
+def program():
   try:
-    token = getToken()
-    if token['name'] != 'PROGRAM':
-      raise SyntaxerError(token, 'PROGRAM')
-
+    terminal('PROGRAM')
     procedureIdentifier()
-    token = getToken()
-    if token['code'] != 59: # ;
-      raise SyntaxerError(token, ';')
-
+    terminal(';')
     block()
-    token = getToken()
-    if token['code'] != 46: # .
-      raise SyntaxerError(token, '.')
+    terminal('.')
 
     if currTokenIndx != len(tokens):
       raise SyntaxerError('Unexpected extra code')
-
     return root
   except SyntaxerError as e:
-    if e.expected == None:
-      print '{}ERROR:{} {}'.format(bcolors.FAIL, bcolors.ENDC, e.value)
-    else:
-      token = e.value
-      print ('{}ERROR:{} Unexpected token \'{}{}{}\' on position {}{}, {}{}. Expected \'{}{}{}\''
-        .format(bcolors.FAIL, bcolors.ENDC, bcolors.BOLD, token['name'], bcolors.ENDC,
-          bcolors.BOLD, token['line'], token['pos'], bcolors.ENDC, bcolors.BOLD, e.expected, bcolors.ENDC))
-      return root
+    return errorHandler(e)
 
 @nonTerminal
 def block():
-  token = getToken()
-  if token['name'] != 'BEGIN':
-    raise SyntaxerError(token, 'BEGIN')
-
+  terminal('BEGIN')
   statementsList()
-  token = getToken()
-  if token['name'] != 'END':
-    raise SyntaxerError(token, 'END')
-
-  return True
+  return terminal('END')
 
 @nonTerminal
 def statementsList():
@@ -113,40 +110,21 @@ def statementsList():
     return addNode('<empty>')
   return True
   
-
 @nonTerminal
 def statement():
   if conditionStatement():
-    token = getToken()
-    if token['name'] != 'ENDIF':
-      raise SyntaxerError(token, 'ENDIF')
-
-    token = getToken()
-    if token['code'] != 59: # ;
-      raise SyntaxerError(token, ';')
-
-    return True
+    terminal('ENDIF')
+    return terminal(';')
 
   rollback(currNode)
-  token = getToken()
-  if token['name'] != 'WHILE':
+  if terminal('WHILE', False) == False:
     return False
 
   conditionalExpression()
-  token = getToken()
-  if token['name'] != 'DO':
-    raise SyntaxerError(token, 'DO')
-
+  terminal('DO')
   statementsList()
-  token = getToken()
-  if token['name'] != 'ENDWHILE':
-    raise SyntaxerError(token, 'ENDWHILE')
-
-  token = getToken()
-  if token['code'] != 59: # ;
-    raise SyntaxerError(token, ';')
-
-  return True
+  terminal('ENDWHILE')
+  return terminal(';')
 
 @nonTerminal
 def conditionStatement():
@@ -154,19 +132,16 @@ def conditionStatement():
 
 @nonTerminal
 def incompleteConditionStatement():
-  if getToken()['name'] != 'IF':
+  if terminal('IF', False) == False:
     return False
 
   conditionalExpression()
-  token = getToken()
-  if token['name'] != 'THEN':
-    raise SyntaxerError(token, 'THEN')
-
+  terminal('THEN')
   return statementsList()
 
 @nonTerminal
 def alternativePart():
-  if getToken()['name'] != 'ELSE':
+  if terminal('ELSE', False) == False:
     rollback(currNode)
     return addNode('<empty>')
   return statementsList()
@@ -177,23 +152,12 @@ def conditionalExpression():
 
 @nonTerminal
 def comparisonOperator():
-  token = getToken()
-  if token['name'] not in comparisons:
-    raise SyntaxerError(token, 'one of them: ' + ', '.join(comparisons))
-  return True
+  return terminal(comparisons)
 
 @nonTerminal
 def expression():
-  token = getToken()
-  code = token['code']
-  if code < 400 or code >= 600:
-    raise SyntaxerError(token, 'identifier or constant')
-  return True
+  return terminal(lambda code: code >= 400 and code < 600, True, 'code', 'identifier or constant')
 
 @nonTerminal
 def procedureIdentifier():
-  token = getToken()
-  code = token['code']
-  if code < 400 or code >= 500:
-    raise SyntaxerError(token, 'identifier')
-  return True
+  return terminal(lambda code: code >= 400 and code < 500, True, 'code', 'identifier')
